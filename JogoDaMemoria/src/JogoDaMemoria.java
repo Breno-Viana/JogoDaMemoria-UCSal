f063e6eaa035e1df91053243b42faf3bc8d20ece
Breno-Viana <150757313+Breno-Viana@users.noreply.github.com>
2024-11-07 17:27:56 -0300

    att

--- JogoDaMemoria/src/JogoDaMemoria.java
+++ JogoDaMemoria/src/JogoDaMemoria.java
@@ -13,10 +13,10 @@ public class JogoDaMemoria {
     static int pares = 0;
     //static boolean[][] VerificarPos = new boolean[4][4];
 
-    static void ImprimirTabuleiro(int Linha, int Coluna) {
+    static void ImprimirTabuleiro() {
         for (int i = 0; i < tabuleiro.length; i++) {
             for (int j = 0; j < tabuleiro[i].length; j++) {
-                System.out.print("X  ");
+                System.out.print("X ");
             }
             System.out.println();
         }
@@ -47,6 +47,13 @@ public class JogoDaMemoria {
     }
 
 
+    static int[][] ChecarPos(int linha, int coluna){
+        for (int i = 0; i <tabuleiro.length ; i++) {
+            for (int j = 0; j <tabuleiro[i].length; j++) {
+                System.out.println(tabuleiro[i][j]);
+            }
+        }
+    }
     static void Inicio() {
         Scanner sc = new Scanner(System.in);
 
@@ -58,22 +65,25 @@ public class JogoDaMemoria {
         System.out.println("==============");
         System.out.println(" EMBARALHADO!");
         Embaralhar();
-        ImprimirTabuleiro(Linha1,Coluna1);
+        ImprimirTabuleiro();
         do {
-            System.out.println("Digite a linha primeira posição (0 - 3) -> ");
-            Linha1 = sc.nextInt();
-            System.out.println("Digite a coluna da primeira posição (0 - 3) ->");
-            Coluna1 = sc.nextInt();
-            ImprimirTabuleiro(Linha1, Coluna1);
+            while (true) {
+                System.out.println("Digite a linha primeira posição (0 - 3) -> ");
+                Linha1 = sc.nextInt();
+                System.out.println("Digite a coluna da primeira posição (0 - 3) ->");
+                Coluna1 = sc.nextInt();
+                if (Linha1 < 0 || Linha1 > 3 && Coluna1 < 0 || Coluna1 > 3){
+                    System.out.println("Posiçoes invalidas\nDigite outras posiçoes");
+                }else {break;}
+            }
+            ChecarPos(Linha1, Coluna1);
             System.out.println("Digite a linha segunda posição (0 - 3) -> ");
             LInha2 = sc.nextInt();
             System.out.println("Digite a coluna da segunda posição (0 - 3) ->");
             Coluna2 = sc.nextInt();
 
         }while (false/*pares != 8*/);
-
-
-
+        System.out.println("Voce ganhou o jogo!!");
     }
     //problema de posição repitida
 
