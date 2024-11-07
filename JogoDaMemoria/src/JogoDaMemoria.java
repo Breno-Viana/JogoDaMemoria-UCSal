808a24091b10936211b1cbaf3ca0d156a1a46df6
Breno-Viana <150757313+Breno-Viana@users.noreply.github.com>
2024-11-07 16:54:16 -0300

    att

--- JogoDaMemoria/src/JogoDaMemoria.java
+++ JogoDaMemoria/src/JogoDaMemoria.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class JogoDaMemoria {
@@ -9,6 +10,7 @@ public class JogoDaMemoria {
             {"A", "B", "C", "D"},
             {"E", "F", "G", "H"}
     };
+    static int pares = 0;
     //static boolean[][] VerificarPos = new boolean[4][4];
 
     static void ImprimirTabuleiro(int Linha, int Coluna) {
@@ -19,6 +21,30 @@ public class JogoDaMemoria {
             System.out.println();
         }
     }
+    static void ImprimirTabu(){
+        for (int i = 0; i < tabuleiro.length; i++) {
+            for (int j = 0; j < tabuleiro[i].length; j++) {
+                System.out.print(tabuleiro[i][j]+"  ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void Embaralhar(){
+        Random Gerar = new Random();
+        int I;
+        int J;
+        for (int i = 0; i <tabuleiro.length ; i++) {
+            for (int j = 0; j <tabuleiro[i].length ; j++) {
+                String temp = tabuleiro[i][j];
+                I = Gerar.nextInt(tabuleiro.length);
+                J = Gerar.nextInt(tabuleiro[i].length);
+                tabuleiro[i][j] = tabuleiro[I][J];
+                tabuleiro[I][J] = temp;
+            }
+
+        }
+    }
 
 
     static void Inicio() {
@@ -26,20 +52,27 @@ public class JogoDaMemoria {
 
         System.out.println("   Jogo da Memoria");
         System.out.println("=====================");
+        ImprimirTabu();
+        System.out.println("Embaralhando....");
+        try{Thread.sleep(3000);}catch (Exception _){}
+        System.out.println("==============");
+        System.out.println(" EMBARALHADO!");
+        Embaralhar();
         ImprimirTabuleiro(Linha1,Coluna1);
-        while (true) {
+        do {
             System.out.println("Digite a linha primeira posição (0 - 3) -> ");
             Linha1 = sc.nextInt();
             System.out.println("Digite a coluna da primeira posição (0 - 3) ->");
             Coluna1 = sc.nextInt();
-            ImprimirTabuleiro(Linha1,Coluna1);
+            ImprimirTabuleiro(Linha1, Coluna1);
             System.out.println("Digite a linha segunda posição (0 - 3) -> ");
             LInha2 = sc.nextInt();
             System.out.println("Digite a coluna da segunda posição (0 - 3) ->");
             Coluna2 = sc.nextInt();
 
+        }while (false/*pares != 8*/);
 
-        }
+
 
     }
     //problema de posição repitida
