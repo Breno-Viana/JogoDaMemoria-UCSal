d2ab68ad399e2c258f1116cc614f6c6e80e606dd
Breno-Viana <150757313+Breno-Viana@users.noreply.github.com>
2024-11-07 18:11:32 -0300

    att

--- JogoDaMemoria/src/JogoDaMemoria.java
+++ JogoDaMemoria/src/JogoDaMemoria.java
@@ -2,8 +2,9 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class JogoDaMemoria {
-    static int Linha1, Coluna1 ;
-    static int LInha2, Coluna2;
+    static int Linha1, Coluna1;
+    static int Linha2, Coluna2;
+    static int RF1 , RF2;
     static String[][] tabuleiro = {
             {"A", "B", "C", "D"},
             {"E", "F", "G", "H"},
@@ -11,17 +12,9 @@ public class JogoDaMemoria {
             {"E", "F", "G", "H"}
     };
     static int pares = 0;
-    //static boolean[][] VerificarPos = new boolean[4][4];
+    static boolean[][] VerificarPos = new boolean[4][4];
 
     static void ImprimirTabuleiro() {
-        for (int i = 0; i < tabuleiro.length; i++) {
-            for (int j = 0; j < tabuleiro[i].length; j++) {
-                System.out.print("X ");
-            }
-            System.out.println();
-        }
-    }
-    static void ImprimirTabu(){
         for (int i = 0; i < tabuleiro.length; i++) {
             for (int j = 0; j < tabuleiro[i].length; j++) {
                 System.out.print(tabuleiro[i][j]+"  ");
@@ -30,12 +23,21 @@ public class JogoDaMemoria {
         }
     }
 
-    static void Embaralhar(){
+    static void ImprimirTabu() {
+        for (int i = 0; i < tabuleiro.length; i++) {
+            for (int j = 0; j < tabuleiro[i].length; j++) {
+                System.out.print(tabuleiro[i][j] + "  ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void Embaralhar() {
         Random Gerar = new Random();
         int I;
         int J;
-        for (int i = 0; i <tabuleiro.length ; i++) {
-            for (int j = 0; j <tabuleiro[i].length ; j++) {
+        for (int i = 0; i < tabuleiro.length; i++) {
+            for (int j = 0; j < tabuleiro[i].length; j++) {
                 String temp = tabuleiro[i][j];
                 I = Gerar.nextInt(tabuleiro.length);
                 J = Gerar.nextInt(tabuleiro[i].length);
@@ -46,14 +48,36 @@ public class JogoDaMemoria {
         }
     }
 
-
-    static int[][] ChecarPos(int linha, int coluna){
-        for (int i = 0; i <tabuleiro.length ; i++) {
-            for (int j = 0; j <tabuleiro[i].length; j++) {
-                System.out.println(tabuleiro[i][j]);
+    static void AbrirPos(int linha, int coluna) {
+        VerificarPos[linha][coluna] = true;
+        for (int i = 0; i < VerificarPos.length; i++) {
+            for (int j = 0; j < VerificarPos[i].length; j++) {
+                if (VerificarPos[i][j]) {
+                    System.out.print(tabuleiro[i][j] + "  ");
+                } else {
+                    System.out.print("X  ");
+                };
             }
+            System.out.println();
         }
     }
+    static void ChecarPos(){
+        if (tabuleiro[Linha1][Coluna1].equals(tabuleiro[Linha2][Coluna2])){
+            VerificarPos[Linha2][Coluna2] = true;
+            AbrirPos(Linha2,Coluna2);
+            pares++;
+        }else {
+            VerificarPos[Linha1][Coluna1] = false;
+            VerificarPos[Linha2][Coluna2]= false;
+            System.out.println("Nao é um par");
+           // AbrirPos();
+        }
+
+
+    }
+
+
+
     static void Inicio() {
         Scanner sc = new Scanner(System.in);
 
@@ -61,7 +85,10 @@ public class JogoDaMemoria {
         System.out.println("=====================");
         ImprimirTabu();
         System.out.println("Embaralhando....");
-        try{Thread.sleep(3000);}catch (Exception _){}
+        try {
+            Thread.sleep(3000);
+        } catch (Exception _) {
+        }
         System.out.println("==============");
         System.out.println(" EMBARALHADO!");
         Embaralhar();
@@ -72,18 +99,28 @@ public class JogoDaMemoria {
                 Linha1 = sc.nextInt();
                 System.out.println("Digite a coluna da primeira posição (0 - 3) ->");
                 Coluna1 = sc.nextInt();
-                if (Linha1 < 0 || Linha1 > 3 && Coluna1 < 0 || Coluna1 > 3){
+                if (Linha1 < 0 || Linha1 > 3 && Coluna1 < 0 || Coluna1 > 3) {
                     System.out.println("Posiçoes invalidas\nDigite outras posiçoes");
-                }else {break;}
+                } else {
+                    break;
+                }
             }
-            ChecarPos(Linha1, Coluna1);
-            System.out.println("Digite a linha segunda posição (0 - 3) -> ");
-            LInha2 = sc.nextInt();
-            System.out.println("Digite a coluna da segunda posição (0 - 3) ->");
-            Coluna2 = sc.nextInt();
-
-        }while (false/*pares != 8*/);
-        System.out.println("Voce ganhou o jogo!!");
+            AbrirPos(Linha1, Coluna1);
+            while (true) {
+                System.out.println("Digite a linha segunda posição (0 - 3) -> ");
+                Linha2 = sc.nextInt();
+                System.out.println("Digite a coluna da segunda posição (0 - 3) ->");
+                Coluna2 = sc.nextInt();
+                if (Linha2 < 0 || Linha2 > 3 && Coluna2 < 0 || Coluna2 > 3) {
+                    System.out.println("Posiçoes invalidas\nDigite outras posiçoes");
+                } else {
+                    break;
+                }
+            }
+            ChecarPos();
+            System.out.println("Pares encontrados - > "+ pares);
+        } while (pares != 8);
+        System.out.println("VOCE GANHOU O JOGO!");
     }
     //problema de posição repitida
 
